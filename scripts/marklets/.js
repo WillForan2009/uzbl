c6f1d9c406a2c66104853e0627de26fcdd3e325c
Will Foran <will.foran@gmail.com>
2010-03-01 23:17:31 -0500

    removed empty bookmarklet

--- scripts/marklets/.js
+++
@@ -1,2 +0,0 @@
-(function(){f=http://delicious.com/save?url=+encodeURIComponent(window.location.href)+&title=+encodeURIComponent(document.title)+&v=5&;a=function(){if(!window.open(f+noui=1&jump=doclose,deliciousuiv5,location=yes,links=no,scrollbars=no,toolbar=no,width=550,height=550))location.href=f+jump=yes};if(/Firefox/.test(navigator.userAgent)){setTimeout(a,0)}else{a()}})()
-
