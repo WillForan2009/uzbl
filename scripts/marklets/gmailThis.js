340ce3890b3ade4c12f6e980d342c3b3d9fe8b39
Will Foran <will.foran@gmail.com>
2010-03-03 04:04:07 -0500

    gmail fixes

--- scripts/marklets/gmailThis.js
+++ scripts/marklets/gmailThis.js
@@ -1,5 +1,5 @@
 (function(){
-m='http://mail.google.com/mail/?view=cm&fs=1&tf=1&to=&su='+encodeURIComponent(document.title)+'&body='+encodeURIComponent(document.location)+' -- ' +encodeURIComponent(window.getSelection());
+m='https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=&su='+encodeURIComponent(document.title)+'&body='+encodeURIComponent(document.location)+' -- ' +encodeURIComponent(window.getSelection());
 window.location=m;
 })();
 
